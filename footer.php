9acb47ea657bafc982dc989dbdac97c1d77754ee
iBzg <isa.baezg@gmail.com>
2018-05-22 22:54:53 -0500

    v2

---
+++ footer.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/estilos.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+    <body>
+            <div class="footer">
+                    <div class="container">
+                            <div class="row">
+                                <div class="col-md-3 col-sm-6 col-xs-12">
+                                    <p class="ft">PaqNos conozas</p>
+                                    <ul>
+                                        <li><a href="">¿Quiénes somos?</a></li>                                    
+                                        <li><a href="">Trabaja con nosotros</a></li>
+                                        <li><a href="">Comentarios y sugerencias</a></li>                
+                                    </ul>
+                                    <a href="https://www.facebook.com/RegalosPaQTQuiera/"><img class="iconos" src="img/fb.png"></a>                
+                                    <a href=""><img class="iconos" src="img/ig.png"></a>
+                                </div>
+                                <div class="col-md-3 col-sm-6 col-xs-12">
+                                    <img src="img/elefante.jpg" alt="" width="100" height="100">
+                                </div>
+                                <div class="col-md-3 col-sm-6 col-xs-12">
+                                    <p class="ft">PaqStes seguro</p>
+                                    <ul>
+                                        <li><a href="">Pago seguro</a></li>                                    
+                                        <li><a href="">Políticas de privacidad</a></li>
+                                        <li><a href="">Políticas de envio</a></li>
+                                        <li><a href="">Políticas de privacidad</a></li>                    
+                                    </ul>
+                                </div>
+                                <div class="col-md-3 col-sm-6 col-xs-12">
+                                    <p class="ft">PaqCpas</p><ul>
+                                        <li><a href="">Preguntas frecuentes</a></li>
+                                        <li><a href="">¿Cómo comprar?</a></li>
+                                        <li><a href="">Solicita tu factura</a></li>
+                                        <p>Horario de atención:
+                                            Lunes a Viernes: 10am - 7pm
+                                        Sábado: 10am - 2pm</p>
+                                        
+                                        <p>Tel: 55-95-38-49</p>
+                                        <p>Email: paqt@paqtquiera.com</p>                 
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="letras">
+                            <img src="paqtLetras.jpg" alt="">
+                        </div>
+            </div>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/estilos.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+        <img src="img/logoPaqtquiera.jpeg" alt="paqtquiera" width="100" height="100">
+        <!--<a class="navbar-brand" href="#">WebSiteName</a>-->
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Menú <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Globos</a></li>
+          <li><a href="#">Peluches</a></li>
+          <li><a href="#">Chocolates</a></li>
+        </ul>
+      </li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></a></li>
+        
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
+  <h3>Navbar With Dropdown</h3>
+  <p>This example adds a dropdown menu for the "Page 1" button in the navigation bar.</p>
+</div>
+</body>
+</html>
